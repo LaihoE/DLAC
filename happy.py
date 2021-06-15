f706da568b3151b74b8929679985ca363ff103b2
PythonBoost <emillaiho@hotmail.fi>
2021-06-15 19:19:50 +0300

    first working csv

--- happy.py
+++ happy.py
@@ -19,7 +19,7 @@ class DataMover():
             self.MatchId = self.x["MatchId"]
             print(self.MatchId)
 
-    def get_pos_player(self, steamid,main_folder):
+    def get_pos_player(self, steamid, main_folder):
         """
         :param steamid:
         :return: None
@@ -27,7 +27,7 @@ class DataMover():
         # Write headers for file
         with open(f"{main_folder}{self.MatchId}.csv",'w',newline='\n')as f:
             thewriter = csv.writer(f)
-            thewriter.writerow(['ActiveWeapon', 'AreaId', 'AreaName', 'Armor', 'DistToBombsiteA',
+            thewriter.writerow(['sus','ActiveWeapon', 'AreaId', 'AreaName', 'Armor', 'DistToBombsiteA',
          'DistToBombsiteB', 'EquipmentValue', 'HasDefuse', 'HasHelmet', 'Hp',
          'Inventory', 'IsAirborne', 'IsAlive', 'IsDucking', 'IsFlashed',
          'IsScoped', 'IsWalking', 'Money', 'Name', 'SteamId', 'TICK',
@@ -63,12 +63,26 @@ class DataMover():
                                 for data in dikt:
                                     writer.writerow(data)"""
 
-
-
     def get_kills_tick(self,kill_folder):
         """
         Gets all the kills in the match
         """
+        # Headers
+        with open(f"{kill_folder}{self.MatchId}.csv",'w',newline='\n')as f:
+            thewriter = csv.writer(f)
+            thewriter.writerow(['sus','AssistedFlash', 'AssisterAreaId', 'AssisterAreaName', 'AssisterName',
+       'AssisterSide', 'AssisterSteamId', 'AssisterTeam', 'AssisterX',
+       'AssisterY', 'AssisterZ', 'AttackerAreaId', 'AttackerAreaName',
+       'AttackerBlind', 'AttackerName', 'AttackerSide', 'AttackerSteamId',
+       'AttackerTeam', 'AttackerViewX', 'AttackerViewY', 'AttackerX',
+       'AttackerY', 'AttackerZ', 'Distance', 'IsFirstKill', 'IsFlashed',
+       'IsHeadshot', 'IsTrade', 'IsWallbang', 'NoScope', 'PenetratedObjects',
+       'PlayerTradedName', 'PlayerTradedSteamId', 'PlayerTradedTeam', 'Second',
+       'ThruSmoke', 'TICK', 'VictimAreaId', 'VictimAreaName', 'VictimName',
+       'VictimSide', 'VictimSteamId', 'VictimTeam', 'VictimViewX',
+       'VictimViewY', 'VictimX', 'VictimY', 'VictimZ', 'Weapon'])
+
+
         for rounds in range(self.n_rounds):
             print(self.x["GameRounds"][rounds]["Kills"][0])
             for k in range(len(self.x["GameRounds"][rounds]["Kills"])):
@@ -77,7 +91,7 @@ class DataMover():
                 df.to_csv(f"{kill_folder}{self.MatchId}.csv", mode='a', header=False)
 
 
-    def split_df_by_kill(self,steamid,out_folder,write_to_csv=False):
+    def split_df_by_kill(self,mainfile,killfile,steamid,out_folder,write_to_csv=False):
         """
         :param attacker_name:
         :return: List of dfs consisting of ticks up to the kill
@@ -85,28 +99,38 @@ class DataMover():
         retruning_list = []
         #df1 = pd.read_sql('csgo2',con=self.dbConnection)
         #df2 = pd.read_sql(f'SELECT * FROM kill2 WHERE AttackerSteamId="{steamid}"',con=self.dbConnection)
-        df1 = pd.read_csv("")
-        df2 = pd.read_csv("")
+        df1 = pd.read_csv(mainfile)
+        df2 = pd.read_csv(killfile)
+
+
         # drop dupes
         df2 = df2.drop_duplicates()
+        print(df1,df2)
         for cnt, x in enumerate(range(len(df2))):
             # Get the next kill and append to list
-            mintick = min(df2["Tick"])
-            out_df = df1[df1['Tick'] <= mintick]
+            mintick = min(df2["TICK"])
+            out_df = df1[df1['TICK'] <= mintick]
             #print(out_df)
             if len(out_df)>0:
                 retruning_list.append(out_df)
                 if write_to_csv == True:
                     out_df.to_csv(f"{out_folder}/{cnt}.csv")
             # Cut off kill from big dfs
-            df1 = df1[df1['Tick'] > mintick]
-            df2 = df2[df2['Tick'] > mintick]
+            df1 = df1[df1['TICK'] > mintick]
+            df2 = df2[df2['TICK'] > mintick]
 
         return retruning_list
 
 
-datamover = DataMover('')
+datamover = DataMover()
 datamover.read_json(r"C:\Users\emill\PycharmProjects\csgoparse\csgo\games/testgame.json")
-datamover.get_pos_player("76561198977257843",f"D:/Users/emill/csgocheaters/games/")
+#datamover.get_pos_player("76561198977257843",f"D:/Users/emill/csgocheaters/games/")
+datamover.get_kills_tick(r'D:\Users\emill\csgocheaters\kills/')
 
-#X = datamover.split_df_by_kill('76561198134270402', r"D:\Users\emill\singlekills", write_to_csv=True)
+
+mainfile = r'D:\Users\emill\csgocheaters\games/testgame.csv'
+killfile = r'D:\Users\emill\csgocheaters\kills/testgame.csv'
+steamid = "76561198977257843"
+out_folder = r"D:\Users\emill\csgocheaters\singlekills/"
+
+X = datamover.split_df_by_kill(mainfile,killfile,steamid,out_folder,write_to_csv=True)
