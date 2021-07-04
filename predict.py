c53431f5c69d044e0d5dd66393904ecd8a5bd4a4
PythonBoost <emillaiho@hotmail.fi>
2021-07-04 14:02:35 +0300

    pred

--- predict.py
+++ predict.py
@@ -90,5 +90,7 @@ if __name__ == "__main__":
     X = X.to(device)
     X = X.float()
 
-    model = torch.load(r"C:\Users\emill\PycharmProjects\CLIP\canmodel/AntiCheat.pt")
-    print(model.predict(X))
+    model = torch.load(r"C:\Users\emill\PycharmProjects\csgoparse\data_analysis/AntiCheat.pt",map_location=torch.device('cpu'))
+    prd = model.predict(X)
+    probs = torch.softmax(prd, 1)
+    print("Probability of cheating:", round(probs[0][0].item(), 2), "        Logits:", prd)
