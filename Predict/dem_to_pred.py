2c2f1aa08de03a0039fb561c77fb63af2e31be35
PythonBoost <emillaiho@hotmail.fi>
2021-12-08 22:11:16 +0200

    new output

--- Predict/dem_to_pred.py
+++ Predict/dem_to_pred.py
@@ -4,8 +4,8 @@ import torch.nn as nn
 import subprocess as sp
 import math
 import numpy as np
-import joblib
-
+import csv
+import copy
 
 is_cuda = torch.cuda.is_available()
 if is_cuda:
@@ -13,6 +13,35 @@ if is_cuda:
 else:
     device = torch.device("cpu")
 print(f"Using: {device}")
+device = 'cpu'
+
+
+class BiGRU(nn.Module):
+    """A pyTorch Bi-Directional LSTM RNN implementation"""
+
+    def __init__(self, embedding_dim, hidden_dim, num_layers, num_classes, batch_size, dropout, device):
+        super(BiGRU, self).__init__()
+        self.hidden_dim = hidden_dim
+        self.batch_size = batch_size
+        self.num_layers = num_layers
+        self.dropout = nn.Dropout(p=dropout)
+        self.lstm = nn.LSTM(
+            input_size=embedding_dim, hidden_size=hidden_dim, num_layers=num_layers,
+            batch_first=True, dropout=dropout, bidirectional=True)
+        self.fc = nn.Linear(hidden_dim * 2, num_classes)
+        self.device = device
+
+    def _init_hidden(self, current_batch_size):
+        h0 = torch.zeros(self.num_layers * 2, current_batch_size, self.hidden_dim)#.to(self.device)
+        c0 = torch.zeros(self.num_layers * 2, current_batch_size, self.hidden_dim)#.to(self.device)
+        return h0, c0
+
+    def forward(self, x):
+        h, c = self._init_hidden(current_batch_size=x.size(0))
+        out, _ = self.lstm(x, (h, c))
+        out = self.dropout(out)
+        out = self.fc(out[:, -1, :])
+        return out
 
 
 class GRUModel(nn.Module):
@@ -24,7 +53,7 @@ class GRUModel(nn.Module):
         self.fc = nn.Linear(hidden_dim, output_dim)
 
     def forward(self, x):
-        h0 = torch.zeros(self.layer_dim, x.size(0), self.hidden_dim).requires_grad_().to(device)
+        h0 = torch.zeros(self.layer_dim, x.size(0), self.hidden_dim).requires_grad_()
         out, _ = self.gru(x, h0.detach())
         out = out[:, -1, :]
         out = self.fc(out)
@@ -32,82 +61,80 @@ class GRUModel(nn.Module):
 
 
 class Model:
-    def __init__(self,df_path):
-        my_scaler = joblib.load(r'C:\Users\emill/scaler.gz')
-        self.df = pd.read_csv(df_path,header=None)
-        print(self.df)
-        self.df = self.df.values  # returns a numpy array
-        print(self.df)
-        self.df = my_scaler.transform(self.df)
-        self.df = pd.DataFrame(self.df)
-        print(self.df)
-        self.X = None
+    def __init__(self,dem_folder,raw=False):
+        self.raw = raw
+        pipe = sp.Popen(f'go run parser.go {dem_folder}', shell=True, stdout=sp.PIPE, stderr=sp.PIPE)
+        res = pipe.communicate()
+        for line in res[0].decode(encoding='utf-8').split('\n'):
+            print(line)
+        self.X = pd.read_csv('data/data.csv', header=None).to_numpy()
+        self.model = torch.load('best.pt', map_location='cpu')
+        self.model.eval()
 
-
-    def extract_features(self):
-        num_samples = len(self.df)
-        data = self.df.to_numpy()
-        X = data[:, 1:]
-        y = data[:, 0]
-
-        features = [x for x in range(24)]
-        num_features = len(features)
-        idxs = []
-        for i in range(X.shape[1]):
-            if (i >= num_features * num_samples):
-                continue
-            if (i % num_features) in features:
-                idxs.append(i)
-
-        X = X.reshape(-1, 256, num_features)
-        return X,y
-
-    def predict(self,batch_size):
-
-        X,y = self.extract_features()
-        #name = X[:,:,0]
-        #id = X[:,:,2]
-        #tick = X[:,:,1]
-
-        # Drop the id and tick since it's not fed into the model
-        #X = X[:,:,3:]
-        # Shape expected is (n,256,24). Example has 20 shots so the shape is (20,256,24)
-        print(f"Shape: {X.shape}")
-        print(X[0])
-        X = X.astype(np.float32)
-        # Deep learning model. Remove map_location if GPU-enabled PyTorch. Enabling GPU speeds up predictions, but may
-        # not be needed if predicting small amounts of games
-        model = torch.load("C:/Users/emill/PycharmProjects/CLIP/canmodel/grus/AntiCheat14.pt",map_location=torch.device('cpu'))
-
-        # rounded up n shots / batch size
+    def predict(self):
+        X = self.X
+        X = X.reshape(-1,128,9)
+        batch_size = 5000
+        file = X[:,:,0]
+        name = X[:,:,1]
+        id = X[:,:,2]
+        tick = X[:,:,3]
+        X = X[:,:,4:]
+        X = np.float32(X)
+        timeline = []
         total_batches = math.ceil(X.shape[0] / batch_size)
-
+        sussies = []
+        detection_dict = {}
+        all_shots_dict = {}
+        # Headers
+        with open('output.csv', 'w', newline='\n')as f:
+            thewriter = csv.writer(f)
+            thewriter.writerow(["SteamId","Total_shots","Detections","Detections/Total_shots"])
         for inx in range(total_batches):
+            print(inx*batch_size)
             x = X[inx * batch_size:inx * batch_size + batch_size, :, :]
-            #ids = id[inx * batch_size:inx * batch_size + batch_size,0]
-            #ticks = tick[inx * batch_size:inx * batch_size + batch_size,:]
-            #names = name[inx * batch_size:inx * batch_size + batch_size]
-            #print(names)
-            x = torch.tensor(x).to(device).float()
-            prd = model.forward(x)
-            probs = torch.softmax(prd, 1)
-            # Each shot in batch
-            for shot in range(x.shape[0]):
-                probability = probs[shot][1].item()
-                # The probabilities are way too confident. For example use 95 % as threshold for a cheating shot.
-                # You can come up with any rule you want, for example if average is over X% or if top 5 predictions are over
-                # X% or even create a ML model on top of these
+            ids = id[inx * batch_size:inx * batch_size + batch_size, :]
+            ticks = tick[inx * batch_size:inx * batch_size + batch_size, :]
+            names = name[inx * batch_size:inx * batch_size + batch_size]
+            files = file[inx * batch_size:inx * batch_size + batch_size]
+            og = copy.deepcopy(x)
+            for i in range(total_batches):
+                x = og[i*batch_size:i*batch_size+batch_size]
+                x = torch.tensor(x).to(device).float()
+                prd = self.model(x)
+                probs = torch.softmax(prd, 1)
+                for shot in range(x.shape[0]):
+                    probability = probs[shot][1].item()
+                    sussies.append((names[shot][0], round(probability, 2) * 100))
+                    timeline.append((names[shot][0],ticks[shot][0],round(probability, 2)))
+                    if ids[shot][0] not in all_shots_dict:
+                        all_shots_dict[ids[shot][0]] = 1
+                    else:
+                        all_shots_dict[ids[shot][0]] += 1
+                    if probability > 0.95 and ids[shot][0] != 0:
+                        if ids[shot][0] not in detection_dict:
+                            detection_dict[ids[shot][0]] = 1
+                        else:
+                            detection_dict[ids[shot][0]] += 1
+                        #print("Name:",names[shot][0],"SteamId:",ids[shot][0],"Cheating:", round(probability, 2)*100, "%         ","Tick:",ticks[shot][0],"File:",files[shot][0])
+                        if self.raw:
+                            with open('cheaters.csv','a',newline='\n',encoding='UTF-8')as f:
+                                thewriter = csv.writer(f)
+                                thewriter.writerow([names[shot][0],ids[shot][0],round(probability, 2)*100,ticks[shot][0],files[shot][0]])
+        preddict = {}
+        for k,v in detection_dict.items():
+            preddict[k] = [all_shots_dict[k],detection_dict[k],detection_dict[k]/all_shots_dict[k]]
 
-                if probability > 0.95:                                        # sampled 64-tick so need to *2 if 128 server
-                    #print("Name",names[shot][0],"SteamID:", str(ids[shot]),"Tick",(ticks[shot,0]* 2), "Cheating:", round(probability, 2)*100, "%")
-                    print("Cheating:", round(probability, 2)*100, "%")
+        # Outputs come from here
+        out = {k: v for k, v in sorted(preddict.items(), key=lambda item: item[1][2], reverse=True)}
+        with open('output.csv', 'a', newline='\n')as f:
+            thewriter = csv.writer(f)
+            for k, v in out.items():
+                print(k,v[0],v[1],v[2])
+                thewriter.writerow([k, v[0], v[1], v[2]])
 
 
 if __name__ == "__main__":
-    dem_folder = './demos2/'
-    pipe = sp.Popen(f'go run parser.go {dem_folder}', shell=True, stdout=sp.PIPE, stderr=sp.PIPE)
-    res = pipe.communicate()
-    for line in res[0].decode(encoding='utf-8').split('\n'):
-        print(line)
-    model = Model("data/data.csv")
-    model.predict(batch_size=100)
+    demo_folder = "./demo/"
+    model = Model(demo_folder)
+    model.predict()
