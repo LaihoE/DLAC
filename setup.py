bd0f1ab17dafa4b7ecf9740fce09b3acdcf29dad
PythonBoost <emillaiho@hotmail.fi>
2021-12-27 16:06:28 +0200

    setup file

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(name='DLAC',
+    version='1.0',
+    description='Deep learning anti-cheat for CSGO',
+    author='LaihoE',
+    packages=find_packages(),
+
+    install_requires=[
+    "numpy==1.21.5",
+    "onnxruntime==1.10.0",
+    "pandas==1.3.5"
+    ],
+     )
