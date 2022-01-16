b4f3310902b480a9a0efdee1aeea682e1b3d1a6f
PythonBoost <emillaiho@hotmail.fi>
2022-01-16 03:57:32 +0200

    new test

--- test.py
+++ test.py
@@ -2,11 +2,3 @@ from DLAC import Model
 
 model = Model("./path_to_demos/")
 model.predict_to_terminal()     # Prints outputs
-
-model.predict_to_terminal(threshold=0.99)   # You can manually specify threshold, 0.95 by default
-
-# You can also get the outputs into a python list
-outputs = model.predict_to_list()
-
-# Or write them into a csv file
-model.predict_to_csv("outputs.csv")
